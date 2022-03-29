6d79e68b02341f65796932ca3d5a7348e28e8a5b
colinsg425 <colinsg425@gmail.com>
2022-03-29 14:33:09 -0700

    Trying

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var playerChoices = ["opener"];//array containing variables
+
+
+var x = document.getElementById("begin");//start button
+var buttons = document.getElementById("mcDonaldsPlayPlace");//refers to button area
+
+var story = {
+    prolog: {
+        text: "hello world!"
+    }
+}
