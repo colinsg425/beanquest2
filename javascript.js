801fce401a2c10d30835b665a4638bbaeb6915e4
Ryan-Vanderville <ryan.vanderville@student.cart.org>
2022-04-05 15:11:17 -0700

    Co-authored-by: colinsg425 <colinsg425@users.noreply.github.com> Co-authored-by: VictorS121504 <VictorS121504@users.noreply.github.com>

--- javascript.js
+++ javascript.js
@@ -1,26 +1,82 @@
 window.onload = function () {
     var story = {
         prolog: {
-            text: "hello world!",
-            options: [["continue", "Continue"]]
+            text: `The year is the far distant future year of 2012. The Great Bean-pression of 1932 led to a great decline in bean sale and production that has yet to be resolved, despite countless attempts from government powers. This led President Hoover to sign an act of Bean repression. The effects of this are shown as beans now have more economical power than the dollar.
+            You are a lonesome plumber, living a peaceful life, left to you and your thoughts alone, and you know nothing of violence, your brother on the other hand has realized the power of violence. You look up to him and thrive to understand and grasp the very concept of the power he wields. You've attempted several times but failed constantly. Numerous times have you tried to rob the elderly lady in apartment room 107, but in your valiant efforts you end up giving her all of your belongings. You continue to work…`,
+            options: [["ditch", "Ditch the Job"], ["knock", "Knock on Door"]]
         },
-        continue: {
-            text: "world hello!",
-            options: [["continue", "Continue"]]
+        ditch: {
+            text: `You leave the job site and head to town, avoiding the monotony of your work life.`,
+        },
+
+        knock: {
+            text: `You are greeted by a client and she invites you to her home. She guides you to her bathroom and you start to get to work.
+
+            “Whoa” you state “these are some dirty pipes”
+            
+            “You want to see something else!” she exclaims
+            
+            You nod with excitement
+            
+            “Ok close your eyes” you hear a rip and the 2 pounds of TNT strapped to her chest detonates.
+            In the rubble all that remains are you and the pipes…
+            
+            “Oh brother” you sigh as you now know you have to get back to work.
+            
+            You return to the pipes. Staring down into the long abyss, before you know it, hours have passed. Amidst the silence and the constant ringing in your ears you reach down the pipe, pulling out the bloated, dead corpse of a blue rat.
+            
+            “Oh brother” You state as you toss aside the lifeless rat into an old rusted bucket.
+             You exclaim “Kobe!”
+            
+            You feel the job is finished yet you feel the urge to double check on your work.`,
+            options: [["finish", "Finish the Job"], ["check", "Check the Pipes Again"]],
+        },
+
+        finish: {
+            text: `You pack up, exit out the front door, and head into town...`,
+        },
+
+        check: {
+            text: `You reach back down the pipe and pull out a can of Bush'sⓇ Baked Beans. (+1 beans)
+            With the pipes now clear, you pack up, exit out the front door, and head into town.`
         }
     };
 
     var playerChoices = ["prolog"];
 
-    var start = document.getElementById("begin");
-    var textArea = document.getElementById("playPlace");
+
+    var x = document.getElementById("begin");
+    var playArea = document.getElementById("playPlace");
     var buttonArea = document.getElementById("buttons");
 
-    function writeStory(text){
-        textArea.innerHTML = text;
+    function createButton(btnText, choice) {
+        var button = document.createElement("button");
+        button.innerHTML = btnText;
+        buttonArea.appendChild(button);
+
+        button.addEventListener("click", function (){
+            playerChoices.push(choice);
+            createStory();
+        });
     }
 
-    start.addEventListener("click", function (){
-        writeStory(story.prolog.text);
+    function addStory(text){
+        playArea.innerHTML = text;
+    }
+
+    function createStory(text) {
+        let pageNow = playerChoices[playerChoices.length - 1];
+        playArea.innerHTML = "";
+        buttonArea.innerHTML = "";
+        for (let idea of playerChoices){
+            addStory(story[idea].text)
+        }
+        for (let idea of story[pageNow].options){
+            createButton(idea[1], idea[0]);
+        }
+    }
+
+    x.addEventListener("click", function () {
+        createStory(story.prolog.text);
     });
 }
